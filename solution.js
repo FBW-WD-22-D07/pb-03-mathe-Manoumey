838a3f69d5f87bd6fb106e0d76437afb91546137
Manoumey <manuella.meyer@dci.education>
2022-12-15 10:22:08 +0100

    first commit

--- solution.js
+++ solution.js
@@ -1,1 +1,111 @@
 // write your code in this file
+
+// write your code in this file
+
+//Q1
+
+console.log("Q1: "+ (6450/15) + " Karten wurden verkauft.");
+
+//Q2
+
+console.log("Q2: Sylvia verdient: " + (500*52) + " im Jahr");
+
+//Q3
+
+console.log("Q3: " + ((17/30)*100) + "%");
+
+//Q4
+
+console.log("Q4: Der Umfang des Quadrats = "+ (4.75*4) +" cm");
+
+//Q5
+
+console.log("Q5: Der Umfang des Dreiecks = " + (5+6+7) + " cm");
+
+//Q6
+
+console.log("Q6: Der Flächeninhalt des Quadrats = "+ (5*5) + " cm²");
+
+//Q7
+
+console.log("Q7: Der Flächeninhalt des rechtwinkligen Dreiecks = "+ (1/2*3*4) + " cm²");
+
+//Q8
+console.log("Q8: Das Volumen des Würfels = " + (9*9*9) + "cm³");
+
+//Q9
+const t1= (22.35) +((22.35*10)/100);
+const t2= (26.67) +((26.67*15)/100);
+const t3=(35.92) +((35.92*20)/100);
+
+
+console.log("Q9: Die drei Rechnungen inklusive Trinkgeld: " + t1 +"; " + t2 +"; " + t3);
+
+//Q10
+console.log("Q10:"+((8+6+5+9+8+2+1+8+5+7+4)/11));
+
+//Q11
+
+const standGesamt=(75+70+85+90+100);
+const standDurchnitt=(standGesamt/5);
+const zielDurchnitt=85;
+const zielGesamt=(zielDurchnitt*6);
+const ereicht=zielGesamt-standGesamt;
+
+console.log("Q11: Score in the sixth test: "+ereicht);
+
+//Q12
+
+const ZielDurchnitt=80;
+const ZielNbPrüf=9;
+const ZielGesamt=(ZielDurchnitt*ZielNbPrüf);
+const StandNbPrüf=8;
+const StandDurchnitt=78;
+const StandGesamt=(StandDurchnitt*StandNbPrüf);
+const Ereicht=ZielGesamt-StandGesamt;
+
+console.log("Q12: James needs a minimum of --% to get an 80% average " + Ereicht);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
